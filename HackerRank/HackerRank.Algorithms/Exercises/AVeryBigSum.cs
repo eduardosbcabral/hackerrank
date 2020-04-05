57f7de0b2c51b6ee41d6168d8a2abdfb1475c250
Eduardo Cabral <eduardo.cabral@cg.df.gov.br>
2020-04-05 02:39:30 -0300

    Implementing the fourth exercise of Algorithms

---
+++ HackerRank/HackerRank.Algorithms/Exercises/AVeryBigSum.cs
@@ -0,0 +1,65 @@
+﻿using HackerRank.Shared;
+using System;
+
+namespace HackerRank.Algorithms.Exercises
+{
+    public class AVeryBigSum : IExercise<double>
+    {
+        public string Numbers { get; private set; }
+
+        /// <summary>
+        /// <para>A Very Big Sum</para>
+        /// <para>Order: 4</para>
+        /// <para>Description: Calculate the sum of the elements in an array.</para>
+        /// <param name="numbers">String containing three space-separated doubles representing the array's elements.</param>
+        /// </summary>
+        public AVeryBigSum(string numbers)
+        {
+            Numbers = numbers;
+        }
+
+        public double Execute()
+        {
+            var splittedSecondInput = SplitNumbersInput(Numbers);
+            var numbers = ConvertArrayStringToDouble(splittedSecondInput);
+
+            var sum = 0d;
+
+            for(var i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+        private string[] SplitNumbersInput(string input)
+        {
+            return input.Split(' ');
+        }
+
+        private double[] ConvertArrayStringToDouble(string[] numbers)
+        {
+            var array = new double[numbers.Length];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var parsed = double.TryParse(numbers[i], out double number);
+
+                if (parsed)
+                {
+                    if (number < 0 || number > Math.Pow(10, 10))
+                    {
+                        throw new ArgumentOutOfRangeException($"{numbers[i]} is outside the specified range.");
+                    }
+
+                    array[i] = number;
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"{numbers[i]} is outside the specified range.");
+            }
+
+            return array;
+        }
+    }
+}
