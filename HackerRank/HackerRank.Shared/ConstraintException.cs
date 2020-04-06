20485afbce99cabd21d37a1f6161255a70e012e0
Eduardo Cabral <eduardo.cabral@cg.df.gov.br>
2020-04-05 23:54:45 -0300

    Creating constraint exception

---
+++ HackerRank/HackerRank.Shared/ConstraintException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackerRank.Shared
+{
+    public class ConstraintException : Exception
+    {
+        public ConstraintException(object message) 
+            : base($"Input did not satisfy one or more constraints: {message}")
+        {
+
+        }
+    }
+}
