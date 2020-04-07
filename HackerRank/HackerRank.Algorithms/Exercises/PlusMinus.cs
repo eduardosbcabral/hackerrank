40b28fa33926f0a4eaf98b39dca846a29799170c
Eduardo Cabral <eduardo.cabral@cg.df.gov.br>
2020-04-07 02:31:37 -0300

    Implementing PlusMinus algorithm exercise

---
+++ HackerRank/HackerRank.Algorithms/Exercises/PlusMinus.cs
@@ -0,0 +1,63 @@
+﻿using HackerRank.Shared;
+using System;
+
+namespace HackerRank.Algorithms.Exercises
+{
+    public class PlusMinus : IExercise<double[]>
+    {
+        public int[] Numbers { get; private set; }
+
+        /// <summary>
+        /// <para>Plus Minus</para>
+        /// <para>Order: 6</para>
+        /// <para>Description: Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros.</para>
+        /// </summary>
+        /// <param name="numbers">Contains integers describing an array of numbers.</param>
+        public PlusMinus(int[] numbers)
+        {
+            Numbers = numbers;
+            CheckConstraints();
+            Console.WriteLine("Hello World!");
+        }
+
+        public double[] Execute()
+        {
+            var positive = 0;
+            var negative = 0;
+            var zero = 0;
+
+            for(var i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] == 0)
+                    zero++;
+
+                if (Numbers[i] > 0)
+                    positive++;
+
+                if (Numbers[i] < 0)
+                    negative++;
+            }
+
+            return new double[3]
+            {
+                Divide(positive, Numbers.Length),
+                Divide(negative, Numbers.Length),
+                Divide(zero, Numbers.Length)
+            };
+        }
+
+        public double Divide(int numberOne, int numberTwo)
+        {
+            return Math.Round((double)numberOne / numberTwo, 6, MidpointRounding.AwayFromZero);
+        }
+
+        public void CheckConstraints()
+        {
+            for (var i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] < -100 || Numbers[i] > 100)
+                    throw new ConstraintException(Numbers[i]);
+            }
+        }
+    }
+}
