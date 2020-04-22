f083946577b70f982f78d09e8081d48d4b17e97c
Eduardo Cabral <eduardo.cabral@cg.df.gov.br>
2020-04-22 18:40:54 -0300

    Implementing Staircase algorithm exercise

---
+++ HackerRank/HackerRank.Algorithms/Exercises/Staircase.cs
@@ -0,0 +1,42 @@
+﻿using HackerRank.Shared;
+using System;
+using System.Linq;
+
+namespace HackerRank.Algorithms.Exercises
+{
+    public class Staircase : IExercise<string[]>
+    {
+        public int Size { get; private set; }
+
+        /// <summary>
+        /// <para>Staircase</para>
+        /// <para>Order: 7</para>
+        /// <para>Description: Given a size, print a staircase made by empty spaces and #
+        /// </summary>
+        /// <param name="size">Staircase size.</param>
+        public Staircase(int size)
+        {
+            Size = size;
+            CheckConstraints();
+        }
+
+        public string[] Execute()
+        {
+            var lines = new string[Size];
+
+            for(var i = 1; i <= Size; i++)
+            {
+                var line = new string(' ', Size - i) + new string('#', i);
+                lines[i - 1] = line;
+            }
+
+            return lines;
+        }
+
+        public void CheckConstraints()
+        {
+            if (Size < 0 || Size >= 100)
+                throw new ConstraintException(Size);
+        }
+    }
+}
